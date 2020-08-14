a78ee0810a68213a51e01edb12dd3a0ed85f18b1
Sean Birmingham <seannbirmingham98@gmail.com>
2020-08-14 19:29:09 -0400

    wrong letters and play again functionality

--- script.js
+++ script.js
@@ -36,7 +36,28 @@ function displayWord() {
 
 // Update the wrong letters
 function updateWrongLettersEl() {
-  console.log('Update wrong');
+  //  Display wrong letters
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+  `;
+
+  // Display parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if (index < errors) {
+      part.style.display = 'block';
+    } else {
+      part.style.display = 'none';
+    }
+  });
+
+  // Check if lost
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = 'Unfortunately, you lost 😢';
+    popup.style.display = 'flex';
+  }
 }
 
 // Show notification
@@ -72,6 +93,21 @@ window.addEventListener('keydown', (e) => {
       }
     }
   }
+});
+
+// Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+  // Empty arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+
+  updateWrongLettersEl();
+
+  popup.style.display = 'none';
 });
 
 displayWord();
